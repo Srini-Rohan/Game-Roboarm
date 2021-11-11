b35712738186567728b795da77dd4e6577d6ee0c
Srini-Rohan <76437900+Srini-Rohan@users.noreply.github.com>
2021-11-11 11:33:37 +0530

    Update and rename hand_mask.py to segment.py

---
+++ segment.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+bg=cv2.imread('bg.jpg',0)
+
+def segment(image, threshold=25):
+    global bg
+    diff = cv2.absdiff(bg.astype("uint8"), image)
+    thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)[1]
+    (cnts, _) = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if len(cnts) == 0:
+        return (thresholded,thresholded)
+    else:
+        segmented = max(cnts, key=cv2.contourArea)
+        return (thresholded, segmented)
+
+
+    
